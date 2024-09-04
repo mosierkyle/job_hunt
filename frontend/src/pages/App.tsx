9e360da36702432c65c8583c705f87ecc7238197
Kyle Mosier <mosierkyle@gmail.com>
2024-09-03 17:46:24 -0700

    Create Axios instance for handling req and res

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './landing';
+import Home from './home';
 import '../global.css';
 
 function App() {
   return (
-    <div className="App">
-      <Landing></Landing>
-    </div>
+    <Router>
+      <div className="App">
+        <Landing></Landing>
+      </div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
