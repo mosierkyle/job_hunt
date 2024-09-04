a78aab149915010401d14e059941f7f0a59a5566
Kyle Mosier <mosierkyle@gmail.com>
2024-09-03 19:38:22 -0700

    Add useAuthenticated hook and protect dashboard page

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
       </Routes>
--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useAuthenticated } from '../../hooks/useAuthenticated';
+import { Navigate } from 'react-router-dom';
 
 const Dashboard: React.FC = () => {
+  const isAuthenticated = useAuthenticated();
+
+  if (isAuthenticated === 'loading') return <div>Loading...</div>;
+  if (isAuthenticated === false) return <Navigate to="/login" />;
+
   return (
     <div>
-      <p>We here</p>
+      <p>Welcome to the Dashboard</p>
     </div>
   );
 };
